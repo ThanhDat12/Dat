0db07b3f343b74f7d999cac06d48eec4bbe2b554
ThanhDat12 <thanhdat8d1@gmail.com>
2021-07-12 09:17:35 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // Declare array a of 10 integers
+    int a[10];
+    // Declare a variable to store the sum of all elements in array
+    int sum = 0;
+    // Use for loop to enter the values of elements in array
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    // Use for loop to find the sum of all elements in array
+    for (int i = 0; i < 10; i++) {
+        sum += a[i];
+    }
+    // Print the sum of all elements in array
+    cout << sum;
+    return 0;
+}
